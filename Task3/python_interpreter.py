02c4fe487f58046043ccb3a3580bdf9bc7b2a690
Ananya Batra <43621901+ananyabatra04@users.noreply.github.com>
2025-01-28 13:05:30 -0800

    Update python_interpreter.py
    Updated recommendation text

--- Task3/python_interpreter.py
+++ Task3/python_interpreter.py
@@ -13,19 +13,17 @@ for result in prolog.query("recommend_book(Book1, Book2)"):
     recommendation2 = result['Book2']
     if recommendation1 and recommendation2 not in recommendations:
         recommendations.add(recommendation1)
-        recommendations.add(recommendation2)
-        print(f"If you liked {result['Book1']}, try {result['Book2']}")
+        print(f"If you liked {result['Book1']}, try {result['Book2']} or vice versa")
 
 # Adding new facts
 prolog.assertz("book('The Catcher in the Rye')")
 prolog.assertz("genre('The Catcher in the Rye', 'Classic')")
 
-# Querying for new recommendations added after adding the new facts
-print("\nNew Book Recommendations generated:")
+# Querying for updated recommendations after adding the new facts
+print("\nUpdated Book Recommendations:")
 for result in prolog.query("recommend_book(Book1, Book2)"):
     recommendation1 = result['Book1']
     recommendation2 = result['Book2']
     if recommendation1 and recommendation2 not in recommendations:
         recommendations.add(recommendation1)
-        recommendations.add(recommendation2)
-        print(f"If you liked {result['Book1']}, try {result['Book2']}")
+        print(f"If you liked {result['Book1']}, try {result['Book2']} or vice versa")
